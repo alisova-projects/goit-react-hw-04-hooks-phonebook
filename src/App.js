1217c78424f97e49c1e6ec4b3cf17e7506623f79
alisova-projects <63472862+alisova-projects@users.noreply.github.com>
2020-12-17 21:00:26 +0200

    up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import react from 'react';
+import Section from './components/Section';
+import Phonebook from './components/Phonebook';
+
+export default function App() {
+  return (
+    <Section>
+      <Phonebook />
+    </Section>
+  );
+}
